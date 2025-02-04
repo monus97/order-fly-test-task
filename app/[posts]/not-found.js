8e0e97bfa080d2adb49ce7d9d0e2a0b057809804
monus97 <monus3871@gmail.com>
2025-02-04 18:57:53 +0530

    working on errors

--- app/[posts]/not-found.js
+++ app/[posts]/not-found.js
@@ -1,6 +1,6 @@
 import style from "@/app/loading.module.css";
 const NotFound = () => {
-  return <div className={style.loading}>Meal Not Found</div>;
+  return <div className={style.loading}>Some-thing Went Wrong</div>;
 };
 
 export default NotFound;
