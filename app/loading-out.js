d311a969039c87340b3f96143ec944a463694808
monus97 <monus3871@gmail.com>
2025-03-07 08:55:29 +0530

    changes

--- app/loading-out.js
+++ app/loading-out.js
@@ -1,6 +1,6 @@
 import style from "@/app/loading.module.css";
 const Loading = () => {
-  return <div className={style.loading}>Loading...</div>;
+  return <div className={style.loading}>Loading...</div>; 
 };
 
 export default Loading;
